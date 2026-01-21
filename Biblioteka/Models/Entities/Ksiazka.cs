b0483ddb6cf8233a942c38c5e66a34446d5e5466
JuliaKornijasz <julka.kornijasz@gmail.com>
2026-01-21 10:53:06 +0100

    Walidacja roku wydania

--- Biblioteka/Models/Entities/Ksiazka.cs
+++ Biblioteka/Models/Entities/Ksiazka.cs
@@ -1,11 +1,14 @@
-﻿namespace Biblioteka.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteka.Models.Entities
 {
     public class Ksiazka
     {
+        
         public int Id { get; set; }
-
+        [Required(ErrorMessage = "Tytuł jest wymagany")]
         public string Tytul { get; set; }
-
+        [Range(1455, 2026, ErrorMessage = "Rok wydania - między 1455 a 2026!")]
         public int RokWydania { get; set; }
 
         // Klucze obce
